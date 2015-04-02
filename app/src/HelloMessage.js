fd859bb77d6acd609f42510e2f77e09b0828748e
Duncan Du <tmduncan60@gmail.com>
2015-04-02 18:08:32 +0800

    upate react compnent

--- app/src/HelloMessage.js
+++ app/src/HelloMessage.js
@@ -2,13 +2,20 @@ import React from 'react';
 module.exports = class HelloMessage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {msg: 'learning use Webpack and reactJs '};
+    this.state = {
+    				msg  : 'learning use Webpack and reactJs',
+					tick : 0
+				};
+  }
+  onClickHandeler() {
+    this.setState({tick : this.state.tick + 1});
   }
   render() {
     return (
     	<div>
     		<h1>Hello {this.props.name}</h1>
     		<p>{this.state.msg}</p>
+    		<p onClick={this.onClickHandeler.bind(this)}>{this.state.tick}</p>
     	</div>);
   }
 }
