20f288bcbb5c6c917a584f1e14fb309ac578aa17
Duncan Du <tmduncan60@gmail.com>
2015-04-08 10:35:26 +0800

    refine make-webpack-config.js

--- make-webpack-config.js
+++ make-webpack-config.js
@@ -6,17 +6,43 @@ var webpack           = require('webpack'),
 	bower_dir         = __dirname + '/app/bower_components';
 
 module.exports = function(options) {
-	var outputPath = options.outputPath,
-		noParse    = [],
-		resolve    = {
-			alias      : {},
-			extensions : ['', '.css', '.scss', '.js']
-		},
-		jshint     = {
-			camelcase  : true,
-			emitErrors : false,
-			failOnHint : false
-		};
+	var outputPath = options.outputPath;
+	var entry = {
+		bundle  : './app/src/main.js',
+		vendors : ['jquery','bootstrap']
+	};
+	var resolve = {
+		alias      : {},
+		extensions : ['', '.css', '.scss', '.js']
+	};
+	var noParse = [];
+	var jshint = {
+		camelcase  : true,
+		emitErrors : false,
+		failOnHint : false
+	};
+	var plugins = [
+		new ReloadPlugin('localhost'),
+		new ExtractTextPlugin('assets/styles/[name].css'),
+		new HtmlWebpackPlugin({
+			filename : 'index.html',
+			template : 'app/index.html'
+		}),
+		new webpack.ProvidePlugin({
+			$                : 'jquery',
+			jQuery           : 'jquery',
+			'windows.jQuery' : 'jquery'
+		})
+	];
+	var loaders = [
+		{ test : /\.(js|jsx)$/, loader: 'babel', include: path.join(__dirname, 'app/src/')},
+		{ test : /\.(woff|ttf|svg|eot)$/, loader: 'url-loader' },
+		{ test : /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
+		{
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader?includePaths[]=' + path.resolve(__dirname, './node_modules/compass-mixins/lib') + '&includePaths[]=' + path.resolve(__dirname, './mixins/app_mixins'))
+        }
+	];
 	var addVendor = function (name, path) {
 		resolve.alias[name] = path;
 		noParse.push(new RegExp('^' + name + '$'));
@@ -28,42 +54,18 @@ module.exports = function(options) {
 	addVendor('bootstrap', bower_dir + '/bootstrap/dist/js/bootstrap.min.js');
 
 	return{
-		entry: {
-			bundle  : './app/src/main.js',
-			vendors : ['jquery','bootstrap']
+		entry   : entry,
+		resolve : resolve,
+		output  : {
+			path     :  outputPath,
+			filename : 'js/[name].js'
 		},
-		resolve: resolve,
-		output: {
-			path              :  outputPath,
-			filename          : 'js/[name].js'
+		plugins : plugins,
+		module  : {
+			noParse : noParse,
+			loaders : loaders
 		},
-		plugins: [
-			new ReloadPlugin('localhost'),
-			new ExtractTextPlugin('assets/styles/[name].css'),
-			new HtmlWebpackPlugin({
-				filename : 'index.html',
-				template : 'app/index.html'
-			}),
-			new webpack.ProvidePlugin({
-				$                : 'jquery',
-				jQuery           : 'jquery',
-				'windows.jQuery' : 'jquery'
-			})
-		],
-		module: {
-			noParse: noParse,
-			loaders: [
-				{ test : /\.(js|jsx)$/, loader: 'babel', include: path.join(__dirname, 'app/src/')},
-				{ test : /\.(woff|ttf|svg|eot)$/, loader: 'url-loader' },
-				{ test : /\.css$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader') },
-				{
-	                test: /\.scss$/,
-	                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!sass-loader?includePaths[]=' + path.resolve(__dirname, './node_modules/compass-mixins/lib') + '&includePaths[]=' + path.resolve(__dirname, './mixins/app_mixins'))
-	            }
-
-			]
-		},
-		jshint: jshint
+		jshint  : jshint
 	}
 }
 
